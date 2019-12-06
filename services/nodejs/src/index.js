2a01df6b81602c228a351b0b66a81d68bca68341
msimou <msimou@ukc02z5a7zlvcf.bskyb.com>
2019-12-06 15:33:34 +0000

    configuring prometheus to monitor nodejs

--- services/nodejs/src/index.js
+++ services/nodejs/src/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
+import {collectDefaultMetrics, register} from 'prom-client'
+import promitheusMiddlewares from './utils/Prometheus'
 
 const app = express(),
     port = process.env.PORT || 8000
 
-app.get('/', (req,res)=> {
-    
+// middlewares
+app.use(promitheusMiddlewares.numbOfRequests)
+app.use(promitheusMiddlewares.sumOfResponseTime)
+app.get('/', (req,res)=> { 
     res.send('alive')
 })
+
+// injects the register to /metrics
+app.get('/metrics', (req,res)=> {register
+    res.set({'Content-Type': register.contentType })
+    res.end(register.metrics())
+})
+// starts the collection
+collectDefaultMetrics({prefix: 'nodejs_',timeout: 5000})
 
 app.listen({port}, () => process.stdout.write(`The app listens on http://localhost:${port}\n`))
---
+++ services/nodejs/src/utils/Prometheus.js
@@ -0,0 +1,33 @@
+import {
+    collectDefaultMetrics,
+    register,
+    Counter,
+    Gauge,
+    Summary
+} from 'prom-client'
+import responseTime from 'response-time'
+
+export const numOfRequests = new Counter({
+    name: 'number_of_requests',
+    help: 'Counts the number of request that the application acccepts'
+})
+export const sumOfResponseTime = new Counter({
+    name: 'sum_of_response_time',
+    help: 'Identifies the time that a request needs',
+})
+ 
+export const numOfRequestsMiddleware = metric => (req,res,next) => {
+    console.log('Middlewares: ', req.path)
+    if(req.path !== '/metrics'){
+        metric.inc(1)
+    }
+    next()
+}
+
+export const sumOfResponseTimeMiddleware = metric => responseTime((req,res,time) => metric.inc(time))
+
+
+export default {
+    numbOfRequests: numOfRequestsMiddleware(numOfRequests),
+    sumOfResponseTime: sumOfResponseTimeMiddleware(sumOfResponseTime),
+}
